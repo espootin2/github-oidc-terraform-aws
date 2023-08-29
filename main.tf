c8d7bcf5d5cda2f0eac1f1ba2822c089a1800640
espootin2 <43667538+espootin2@users.noreply.github.com>
2023-08-29 09:52:05 +0200

    add new resource

--- main.tf
+++ main.tf
@@ -1,5 +1,27 @@
-resource "aws_ssm_parameter" "foo" {
-  name  = "foo"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-04e601abe3e1a910f"
+  instance_type = "t2.micro"
+  subnet_id = "subnet-0d77b25fd56e5cce0"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
+
+resource "aws_ssm_parameter" "foo2" {
+  name  = "foo2"
   type  = "String"
-  value = "barr"
+  value = "barr2"
 }
